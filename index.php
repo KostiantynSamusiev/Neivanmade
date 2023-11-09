17c9e1fb2d8a9bff0ae3cd20bd4265b7a210fe0f
Kostiantyn Samusiev <djfoozyhook@gmail.com>
2023-11-09 23:54:42 +0200

    asd

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
   <!-- PRESENTATION  -->
   <div class="presentation" id="presentation">
     <!-- <video src="<?php echo bloginfo('template_url'); ?>" autoplay="" loop="" playsinline="" muted=""></video> -->
-    <img src="<?php echo bloginfo('template_url'); ?>/assets/img/main-pic.webp"></img>
+    <!-- <img src="<?php echo bloginfo('template_url'); ?>/assets/img/main-pic.webp"></img> -->
     <div class="text">#NEIVANMADE</div>
   </div>
 
@@ -217,21 +217,21 @@ $counter = 0;
         $counter++;
 
         ?>
-						      <a class="exhb-link" href="<?php echo get_permalink(get_page_by_path('exhibitions')); ?>">
-						        <div class="block">
-						          <div class="block__number"> <?php echo "/0" . $counter; ?></div>
-						          <div class="block__info">
-						            <div class="name"><?php the_title()?></div>
-						            <div class="gallery"><?php echo the_excerpt(); ?></div>
-						          </div>
-						          <div class="block__icon">
-						            <img data-src="<?php echo bloginfo('template_url'); ?>/assets/img/exhibition-icon.svg" alt="">
-						          </div>
-						        </div>
-						      </a>
+							      <a class="exhb-link" href="<?php echo get_permalink(get_page_by_path('exhibitions')); ?>">
+							        <div class="block">
+							          <div class="block__number"> <?php echo "/0" . $counter; ?></div>
+							          <div class="block__info">
+							            <div class="name"><?php the_title()?></div>
+							            <div class="gallery"><?php echo the_excerpt(); ?></div>
+							          </div>
+							          <div class="block__icon">
+							            <img data-src="<?php echo bloginfo('template_url'); ?>/assets/img/exhibition-icon.svg" alt="">
+							          </div>
+							        </div>
+							      </a>
 
 
-						      <?php
+							      <?php
 endwhile;
     wp_reset_postdata();
     ?>
