51a94238b2088a72e7ea922107926179c60f01b0
Kostiantyn Samusiev <djfoozyhook@gmail.com>
2023-11-15 14:11:50 +0200

    fix JS

--- src/js/modules/exhibitions-block-animations.js
+++ src/js/modules/exhibitions-block-animations.js
@@ -1,29 +1,31 @@
 function exhibitionAnimation() {
     //Exhebitions blocks
     $(window).on("click", function (e) {
-        let content = $(e.target).parent().find('#content p');
         let target = $(e.target).find('.icon');
         let parentBlock = $(e.target).closest('.exhibitions__year-block');
         let blockInfo = $(e.target).closest('.block-info');
         let exContainer = $(e.target).closest('.exhibitions-container');
         let year = e.target.closest('.year-info');
-        let td = e.target.closest('.exhibitions-container');
+        let td = e.target.closest('.block');
 
         if (e.target.classList.contains('year-info')) {
             console.log('YEAR')
+            target.toggleClass("rotate");
             parentBlock.find('.block-info').fadeToggle("slow");
         }
 
+        let content = exContainer.find('#content p');
         // Cheking exhibition block
         var div = content.length;
-        if (div) {
+        if (exContainer || td) {
+            console.log('works')
 
-            target.toggleClass("rotate");
-
-            else if (exContainer) {
-                console.log('CONTAINER')
+            if (div) {
+                target.find('.icon').toggleClass("rotate");
+                exContainer.find('.icon').toggleClass("rotate");
                 $(exContainer).find('.inner-block').fadeToggle("slow");
             }
+
         }
     });
 
@@ -32,8 +34,6 @@ function exhibitionAnimation() {
     if (fristBlock) {
         fristBlock.classList.add("rotate");
     };
-
-
 
 
 
